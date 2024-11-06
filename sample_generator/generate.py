a96aee1cd6913a0c8854c1f67b94af6ae49063bf
Dmitri Soshnikov <dmitri@soshnikov.com>
2024-11-06 22:14:33 +0300

    Add datasphere generator

--- sample_generator/generate.py
+++ sample_generator/generate.py
@@ -4,21 +4,27 @@ from ycloud import *
 from hsecloud import *
 import functools as funt
 import time
+import uuid
 
 output_dir = '../samples'
-skip_if_exists = True
 
 with open('description.json',encoding='utf-8') as f:
     desc = json.load(f)
 
 models = {
     'YA' : ("Yandex ART","_ru",submit_art,check),
+    'sd2' : ("Stable Diffusion 2","",funt.partial(submit_hse,model="sd2"),check_hse),
     'sdxl' : ("Stable Diffusion XL","",funt.partial(submit_hse,model="sdxl"),check_hse),
-    'k22' : ("Kandinsky","",funt.partial(submit_hse,model="k22"),check_hse),
+    'k22' : ("Kandinsky 2","",funt.partial(submit_hse,model="k22"),check_hse),
+    'flux' : ("Flux","",None,None),
+    'k3' : ("Kandinsky 3","",None,None),
 }
 
+jobs = []
+metaindex = {}
 for obj in desc:
     cat = obj['category']
+    title = obj['title']
     dir = cat[:cat.rfind('/')]
     cat = cat[cat.rfind('/')+1:]
     print(f" + Processing category {dir} -> {cat}")
@@ -42,10 +48,14 @@ for obj in desc:
                     break
                 items[mname][p] = items[mname].get(p,[]) + [fname]
                 i+=1
-            if i>0 and skip_if_exists:
+            if i>0:
                 skip += 1
             else:
-                id = submit_fn(p)
+                if submit_fn:
+                    id = submit_fn(p)
+                else:
+                    id = uuid.uuid4()
+                    jobs.append({ "model" : m, "prompt" : p, "fname" : os.path.join(dir,fname) })
                 sub += 1
                 queue[id] = (check_fn,f"{m}_{p.replace(' ','_')}_{{}}.png",mname,p)
         print(f'{sub} submitted, {skip} skipped')
@@ -55,7 +65,11 @@ for obj in desc:
         queue = { k:v for k,v in queue.items() if v is not None }
         if len(queue) == 0:
             break
-        for id,(fun,fname,mname,p) in queue.items(): 
+        for id,(fun,fname,mname,p) in queue.items():
+            if fun is None:
+                queue[id] = None
+                items[mname][p] = [fname.format(0)]
+                continue
             if res:=fun(id):
                 for i,im in enumerate(res):
                     im.save(os.path.join(output_dir,dir,fname.format(i)))
@@ -63,14 +77,24 @@ for obj in desc:
                 print('.',end='',flush=True)
                 queue[id] = None
     print("..done")
+    with open('jobs.json','w',encoding='utf-8') as f:
+        json.dump(jobs,f)
     print(" + Generating markdown")
-    with open(os.path.join(output_dir,dir,'index.md'),'w',encoding='utf-8') as f:
-        f.write(f"## {cat}\n\n")
+    metaindex[os.path.join(dir,'index.md')] = title 
+    with open(os.path.join(output_dir,dir,'index.md'),'a',encoding='utf-8') as f:
+        f.write(f"## {title} ({cat})\n\n")
         for mname, v in items.items():
             f.write(f"### {mname}\n\n")
             l = list(v.values()) 
             for i in range(len(l[0])):
                 f.write(' | '.join([f"![]({x[i]})" for x in l])+'\n')
                 if i==0:
-                    f.write('|'.join(['-----']*len(l[0]))+'\n')
+                    f.write('|'.join(['-----']*len(v.values()))+'\n')
             f.write(' | '.join(v.keys())+'\n\n')
+print(" + Saving metaindex")
+with open(os.path.join(output_dir,'metaindex.md'),'w',encoding='utf-8') as f:
+    f.write(f'## Metaindex\n\n')
+    for p,t in metaindex.items():
+        f.write(f" * [{t}]({p})\n")
+print(" + Done")
+
